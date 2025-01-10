9aef9825ac3e6305ebfadabf6c90de579e3f8c2b
Nick Lemoncito <108027865+nicklm0@users.noreply.github.com>
2025-01-10 16:38:01 +0800

    Update Microsoft.PowerShell_profile.ps1

--- Powershell_configs/Microsoft.PowerShell_profile.ps1
+++ Powershell_configs/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-#starshipt promt
+#starshipt prompt 
 Invoke-Expression (&starship init powershell)
 
 #disable autopredict 
@@ -7,6 +7,29 @@ Set-PSReadLineOption -PredictionSource None
 # Import-Module PSReadLine
 Remove-Module PSReadLine
 
+#Fzf install
+#scoop install fzf
+#Install-Module -Name PSFzf -Scope CurrentUser -Force
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+#terminal icons: Install-Module -Name Terminal-Icons -Repository PSGallary -Force
+#Import-Module Terminal-Icons
+Import-Module -Name Terminal-Icons
+
 # Alias
 Set-Alias -Name vim -Value nvim
 Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+
+
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
